4ed412c491274d92aa65e9ebb412a822a618a857
thee <1579290423@qq.com>
2025-02-16 15:29:41 +0800

    feat(kernel): 添加异常处理程序支持
    1.添加简单异常处理程序并进入内核恐慌
    2.更新异常处理的笔记

--- kernel.h
+++ kernel.h
@@ -1,12 +1,50 @@
 #ifndef __KERNEL_H
 #define __KERNEL_H
 
+#include "common.h"
+
 // 返回值结构体
 struct sbiret
 {
 	long error;
 	long value;
 };
+
+// 异常处理程序状态结构体
+struct trap_frame
+{
+	uint32_t ra;
+	uint32_t gp;
+	uint32_t tp;
+	uint32_t t0;
+	uint32_t t1;
+	uint32_t t2;
+	uint32_t t3;
+	uint32_t t4;
+	uint32_t t5;
+	uint32_t t6;
+	uint32_t a0;
+	uint32_t a1;
+	uint32_t a2;
+	uint32_t a3;
+	uint32_t a4;
+	uint32_t a5;
+	uint32_t a6;
+	uint32_t a7;
+	uint32_t s0;
+	uint32_t s1;
+	uint32_t s2;
+	uint32_t s3;
+	uint32_t s4;
+	uint32_t s5;
+	uint32_t s6;
+	uint32_t s7;
+	uint32_t s8;
+	uint32_t s9;
+	uint32_t s10;
+	uint32_t s11;
+	uint32_t sp;
+} __attribute__((packed));
 
 // 内核恐慌 Kernel Panic
 #define PANIC(fmt, ...)                                                      \
@@ -17,5 +55,21 @@ struct sbiret
 		{                                                                    \
 		}                                                                    \
 	} while (0)
+
+// 读取CSR寄存器
+#define READ_CSR(reg)                                         \
+	({                                                        \
+		unsigned long __tmp;                                  \
+		__asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp)); \
+		__tmp;                                                \
+	})
+
+// 写入CSR寄存器
+#define WRITE_CSR(reg, value)                                     \
+	do                                                            \
+	{                                                             \
+		uint32_t __tmp = (value);                                 \
+		__asm__ __volatile__("csrw " #reg ", %0" : : "r"(__tmp)); \
+	} while (0);
 
 #endif
